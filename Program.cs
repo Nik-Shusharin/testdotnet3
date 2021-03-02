4c8b3513e945b02b6dddd8f249fed9a2a98f6c9c
unknown <nshusharin@servicetitan.com>
2021-03-02 13:19:54 -0800

    pr01titan

--- Program.cs
+++ Program.cs
@@ -78,5 +78,20 @@ namespace testdotnet
                 return reader.ReadToEnd();
             }
         }
+
+        public static string GetRequest3()
+        {
+            var MyToken= "asdfERWQEFASDFAWERWR345FASDFAS34fASFDADSFASDF34rqwefSAFDSADFA";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.google.com");
+            request.Headers.Add("authorization", "Bearer asdfERWQEFASDFAWERWR345FASDFAS34fASFDADSFASDF34rqwefSAFDSADFA");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
     }
 }
